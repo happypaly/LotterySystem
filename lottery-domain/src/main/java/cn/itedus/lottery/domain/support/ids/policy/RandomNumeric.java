a08d868b52fadeb87f494a61e82fcac345645186
KongJuan <kongjuan_k@163.com>
2022-02-20 13:49:05 +0800

    ID生成策略领域开发

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/support/ids/policy/RandomNumeric.java
@@ -0,0 +1,18 @@
+package cn.itedus.lottery.domain.support.ids.policy;
+
+import cn.itedus.lottery.domain.support.ids.IIdGenerator;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * 工具类生成 org.apache.commons.lang3.RandomStringUtils
+ */
+@Component
+public class RandomNumeric implements IIdGenerator {
+
+    @Override
+    public long nextId() {
+        return Long.parseLong(RandomStringUtils.randomNumeric(11));
+    }
+
+}
