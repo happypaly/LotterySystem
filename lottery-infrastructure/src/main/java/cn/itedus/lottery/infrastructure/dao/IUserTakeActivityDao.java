02961bfa78fba93b326d0303013c37bbddca9c65
KongJuan <kongjuan_k@163.com>
2022-02-20 20:52:13 +0800

    实现和使用分库分表

---
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IUserTakeActivityDao.java
@@ -0,0 +1,21 @@
+package cn.itedus.lottery.infrastructure.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.itedus.lottery.infrastructure.po.UserTakeActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description: 用户领取活动表DAO
+ */
+@Mapper
+public interface IUserTakeActivityDao {
+
+    /**
+     * 插入用户领取活动信息
+     *
+     * @param userTakeActivity 入参
+     */
+    @DBRouter(key = "uId")
+    void insert(UserTakeActivity userTakeActivity);
+
+}
